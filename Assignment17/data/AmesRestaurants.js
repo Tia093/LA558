f2c6b42c83c93acb91e357a2ed4468fa95cfe38c
Tia093 <35468029+Tia093@users.noreply.github.com>
2018-04-03 19:05:58 -0500

    Create AmesRestaurants.js

---
+++ Assignment17/data/AmesRestaurants.js
@@ -0,0 +1,22 @@
+var AmesR=[
+[42.0005632,-93.6356789,"Provisons Lot F"],
+[42.001077,	-93.643007,"The IowaStater Restaurant"],
+[42.0080436,	-93.614684,	"Dublin Bay Irish Pub& Grill"],
+[42.006972,	-93.611761,	"Fuji Japanese Steak House"],
+[42.0100315,	-93.6094406,	"Hickory Park"],
+[42.011974,	-93.610983,	"Applebee`s Grill+Bar"],
+[42.015537,	-93.611294,	"HuHot Mongolian Grill"],
+[42.019296,	-93.610939,	"Perkins Restaurant & Bakery"],
+[42.021424,	-93.64919,	"Thai Kitchen"],
+[42.024016,	-93.618754,	"Culver`s"],
+[42.023179,	-93.6161,	"Mandarin Chinese Restaurant"],
+[42.025417,	-93.619049,	"Aunt Maudes"],
+[42.024931,	-93.614736,	"Olde Main Brewing Co"],
+[42.025066,	-93.61142,	"The Grove Café"],
+[42.05007,	-93.645568,	"El Azteca"],
+[42.04882,	-93.643672,	"The Café"],
+[42.050318,	-93.645442,	"Wallaby`s Bar and Grille"],
+[42.054939,	-93.623077,	"Ge-Angelo`s"],
+[42.054981,	-93.622001,	"888"],
+[42.013265,	-93.610787,	"Red Lobster"]
+];
